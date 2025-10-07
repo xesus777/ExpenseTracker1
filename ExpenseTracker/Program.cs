8fc33f15b141aaa9829434af2ae0707ef01908bb
234827 <234827@KB37-303-C10>
2025-10-07 13:19:12 +0300

    Декомпозиция, Добавлен класс Book и его конструктор и перечисление для жанров Genre

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibraryManagement
+{
+    
+    public enum Genre
+    {
+        Fiction,
+        Science,
+        History,
+        Fantasy,
+        Mystery
+    }
+
+    
+    public class Book
+    {
+        
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public Genre Genre { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+
+        
+        public Book(int id, string title, string author, Genre genre, int year, decimal price)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+            Genre = genre;
+            Year = year;
+            Price = price;
+        }
+
+        
+        
+    }
+
+    
+}
