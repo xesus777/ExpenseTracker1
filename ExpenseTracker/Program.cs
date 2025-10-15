095c971ca84e378cdb051f761b9341862889815a
Litt22 <zdanovicgleb@gmail.com>
2025-10-15 17:51:24 +0300

    Декомпозиция, Добавлен класс Person

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -1,1 +1,27 @@
 ﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UniversityManagementSystem
+{
+    
+    public enum Department
+    {
+        ComputerScience,
+        Mathematics,
+        Physics,
+        Engineering,
+        Business,
+        Arts
+    }
+
+    // Базовый класс для всех людей 
+    public abstract class Person
+    {
+        
+
+        
+    }
+
+    
