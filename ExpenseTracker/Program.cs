51ed31d7e3fc3842882a311045043782fc145f4b
234827 <234827@KB37-303-C9>
2025-10-21 09:43:51 +0300

    Заполнен класс Player - добавлены свойства (здровье имя и тп) + базовые методы по здоровью и тп

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -6,7 +6,49 @@ namespace TextRoguelike
 {
     public class Player
     {
-        
+        public string Name { get; set; } = "Игрок";
+        public int Health { get; set; }
+        public int MaxHealth { get; set; }
+        public int BaseAttack { get; set; }
+        public int BaseDefense { get; set; }
+        public Weapon EquippedWeapon { get; set; }
+        public Armor EquippedArmor { get; set; }
+        public bool IsFrozen { get; set; }
+        public bool IsDefending { get; set; }
+
+        public int TotalAttack => BaseAttack + (EquippedWeapon?.AttackBonus ?? 0);
+        public int TotalDefense => BaseDefense + (EquippedArmor?.DefenseBonus ?? 0);
+        public bool IsAlive => Health > 0;
+
+        public Player()
+        {
+            Health = 100;
+            MaxHealth = 100;
+            BaseAttack = 10;
+            BaseDefense = 5;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+            if (Health < 0) Health = 0;
+        }
+
+        public void Heal(int amount)
+        {
+            Health += amount;
+            if (Health > MaxHealth) Health = MaxHealth;
+        }
+
+        public void FullHeal()
+        {
+            Health = MaxHealth;
+        }
+
+        public void ResetDefense()
+        {
+            IsDefending = false;
+        }
     }
 
     public class Enemy
