f21a8f2eff4c0d286b8489679dde77fca61cc068
234827 <234827@KB37-303-C9>
2025-10-21 09:47:29 +0300

    заполнен класс Item - свойства предметов (класс родитель)

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -103,7 +103,9 @@ namespace TextRoguelike
 
     public class Item
     {
-        
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; } 
     }
 
     public class Weapon : Item
